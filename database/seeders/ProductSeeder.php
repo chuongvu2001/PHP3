e7da4fe1e50ac580fe93b74de828288653731da6
thienth32 <thienth32@gmail.com>
2021-01-20 13:31:32 +0700

    update

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -3,7 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-
+use Faker;
+use App\Models\Product;
 class ProductSeeder extends Seeder
 {
     /**
@@ -13,6 +14,18 @@ class ProductSeeder extends Seeder
      */
     public function run()
     {
+        $faker = Faker\Factory::create();
+        for($i = 0; $i < 100; $i++){
+            $model = new Product();
+            $model->fill([
+                'name' => $faker->name,
+                'price' => $faker->numberBetween($min = 1000, $max = 10000000),
+                'cate_id' => rand(9, 30),
+                'detail' => $faker->realText(150, 2),
+                'image'=> "images/anh-".rand(1, 4).".jpg"
+            ]);
+            $model->save();
+        }
         //
     }
 }
